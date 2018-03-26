078865ab660ec06a6ab8f26c27fc017e8e6b4b0e
Robin Toll <toll.robin@gmail.com>
2018-03-25 23:18:26 -0400

    Initial Commit

---
+++ TestApp/TestProg.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Toll.K47.TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Application Start: {0}\n-----------------\n", DateTime.Now);
+
+            XDocument xd = XDocument.Load("K47Base.xml");
+
+            Console.WriteLine("***");
+            foreach (XElement xe in xd.Elements())
+                Console.WriteLine(xe.ToString());
+            Console.WriteLine("***");
+
+            Console.WriteLine("\n\n---------------\nApplication End: {0}", DateTime.Now);
+        }
+    }
+}
